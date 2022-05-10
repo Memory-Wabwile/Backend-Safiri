f18b9d8437d71bdefd2ed63a908d147abb4f1ef7
ELIJAH KIRAGU <eliki13720@gmail.com>
2022-05-10 18:04:23 +0300

    checking the error

--- customers/serializer.py
+++ customers/serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import BusBooking
 
 # class BookingSerializer(serializers.ModelSerializer):
-#     class Meta:
+#    
 #         model = Booking
 #         fields = '__all__'
 
